d48ce8c192e954e416d414f45a11502003c2bf08
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-02-17 14:36:05 +0100

    fixtures user with faker

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
 
 class UserFixtures extends Fixture
 {
@@ -18,12 +19,14 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         for ($i = 1; $i <= 2; $i++) {
             $userAdmin = new User();
             $password = $this->hasher->hashPassword($userAdmin, 'password');
 
-            $userAdmin->setUsername('admin' . $i)
-                ->setEmail('admin' . $i . '@gmail.com')
+            $userAdmin->setUsername($faker->name())
+                ->setEmail($faker->unique()->email())
                 ->setPassword($password)
                 ->setRoles(['ROLE_ADMIN']);
 
@@ -34,8 +37,8 @@ class UserFixtures extends Fixture
             $user = new User();
             $password = $this->hasher->hashPassword($user, 'password');
 
-            $user->setUsername('user' . $i)
-                ->setEmail('user' . $i . '@gmail.com')
+            $user->setUsername($faker->name())
+                ->setEmail($faker->unique()->email())
                 ->setPassword($password);
 
             $manager->persist($user);
