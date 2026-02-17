dcd2d1d3db502f8659be5489224001fd9116ab2b
Elisee Desmarest <elisee.desmarest@gmail.com>
2026-02-17 16:15:19 +0100

    make test for CategoryRepository

---
+++ tests/Repository/CategoryRepositoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Repository;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CategoryRepositoryTest extends KernelTestCase
+{
+    public function testFindAllCategory(): void
+    {
+        self::bootKernel();
+        $container = self::getContainer();
+
+        $categories = count($container->get(CategoryRepository::class)->findAll());
+        $this->assertEquals(6, $categories);
+    }
+
+    public function testFindOneByTitleCategory(): void
+    {
+        self::bootKernel();
+        $container = self::getContainer();
+
+        $category[] = $container->get(CategoryRepository::class)->findOneBy(['title' => 'Chaussure']);
+        $this->assertEquals(1, count($category));
+    }
+}
